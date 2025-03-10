d443eaa2fd250ebe6ab3489d76a48375526a3bf4
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-03-11 00:07:39 +0200

    Create Views

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,21 @@ from .models import Account
 
 class RegisterForm(forms.ModelForm):
 
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
 
     class Meta:
         model = Account
-        fields = ['first_name' , 'last_name' , 'username' , 'country' , 'email' , 'phone_number' , 'password']
+        fields = ['first_name' , 'last_name' , 'country' , 'email' , 'phone_number' ]
+
+
+    def clean(self):
+        cleaned_data = super(RegisterForm , self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from .forms import RegistrationForm
+from .models import Account
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            country = form.cleaned_data['country']
+            password = form.cleaned_data['password']   
+            email = form.cleaned_data['email'] 
+            username = email.split('@')[0]
+            phone_number = form.cleaned_data['phone_number']
+
+            user = Account.objects.create_user(first_name=first_name , last_name=last_name , username=username , country=country , email=email , password=password)
+            user.phone_number = phone_number
+            user.save()
