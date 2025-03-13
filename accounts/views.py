2e0f763a7f21727a8bfb99c63bcefcf00373980e
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-03-13 17:18:24 +0200

    Activation email

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
 from .models import Account
-# Create your views here.
 
+# Activation Email  عشان اعمل اكتيف لليوزر بعد التسجيل
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode , urlsafe_base64_decode
+from django.utils.encoding import force_bytes , force_text
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +28,18 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name , last_name=last_name , username=username , country=country , email=email , password=password)
             user.phone_number = phone_number
             user.save()
+
+            #User Activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please Activate Your Account'
+            message = render_to_string('accounts/account_verification_email.html' ,{
+                'user': user ,
+                'domain': current_site ,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_mail = EmailMessage(mail_subject , message , to=[to_email])
+            send_mail.send()
+            return redirect('login'+f'?command=verification&email={email}')
+
