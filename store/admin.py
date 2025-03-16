29117c5a8ae18fac3a026123147d9582446f82f2
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-03-17 00:49:32 +0200

    Postgresql DB

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
