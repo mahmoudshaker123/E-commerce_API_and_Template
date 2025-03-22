8c13dccd5809ad45979b5002dcc000810fc08ee2
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-03-23 00:57:04 +0200

    Iterate Over Cart Items and Fetch Products from the Database

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-
+from decimal import Decimal
+from store.models import Product
 
 
 class Cart:
@@ -30,3 +31,15 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
