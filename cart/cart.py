ebc08cca611ef18ce03f54d36b4ac4fc3da50b38
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-03-23 00:43:37 +0200

    Create Add Product Method in Cart App

--- cart/cart.py
+++ cart/cart.py
@@ -10,3 +10,18 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
          
+    def add(self , product , quantity=1 , update_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id]= {'quantity':0 , 'price':str(product.price)}
+
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
