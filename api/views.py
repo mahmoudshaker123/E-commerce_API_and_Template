7a02fae78cb304d993fdbfb69c88f8cee1295a17
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-04-03 02:17:24 +0200

    Create Product

--- api/views.py
+++ api/views.py
@@ -8,18 +8,37 @@ from .serializers import CategorySerializer  , ProductSerializer
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET' ,'POST'])
 def category_api(request):
     if request.method == 'GET':
         categories = Category.objects.all()
         serializer  = CategorySerializer(categories , many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
     
+    elif request.method =='POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Object Post successfuly'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
     
-@api_view(['GET'])
+
+        
+
+    
+    
+@api_view(['GET' , 'POST'])
 def product_api(request):
     if request.method == 'GET':
         products = Product.objects.filter(status=Product.Status.AVAILABLE)
         serializer = ProductSerializer(products , many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
+    
+    elif request.method =='POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    
     
